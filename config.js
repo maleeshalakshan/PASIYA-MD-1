151fee6a05d9ab6ed597605a356071e0d1b3d13e
Maleesha Lakshan <109362145+maleeshalakshan@users.noreply.github.com>
2025-01-15 15:49:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "2E9nlSiZ#ff9bgltiZNrgnxZ7lQrePWu6WWt7xQenDrg-ZqwQIAQ",
+SESSION_ID: process.env.SESSION_ID || "2nB1VQbQ#cYF7BcH7D4D5FxEZ5HU6W9zFpJQs-2n1xLP3-I_sZKY",
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya",
 MODE: process.env.MODE || "public",  
 PREFIX: process.env.PREFIX || ".",   

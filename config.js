33958eb274c429018ebcfa46918d402842afc32a
Maleesha Lakshan <109362145+maleeshalakshan@users.noreply.github.com>
2025-01-15 14:46:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "C2whiTpJ#G7oJ557CYtp5AHjar8YnC8KFreGE4o-XwuxtBhLRy-o",
+SESSION_ID: process.env.SESSION_ID || "2E9nlSiZ#ff9bgltiZNrgnxZ7lQrePWu6WWt7xQenDrg-ZqwQIAQ",
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya",
-MODE: process.env.MODE || "private",  
-PREFIX: process.env.PREFIX || ",",   
+MODE: process.env.MODE || "public",  
+PREFIX: process.env.PREFIX || ".",   
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg", 
 ALIVE_MSG: process.env.ALIVE_MSG || "HI AME WHATSAPP BOT", 
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya",
-PREFIX: process.env.PREFIX || ",",   
+PREFIX: process.env.PREFIX || ".",   
 };

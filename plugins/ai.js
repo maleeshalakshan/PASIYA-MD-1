446e34ffbcca4bfeedd5d6b097b7d29b3c4dc0bb
Maleesha Lakshan <109362145+maleeshalakshan@users.noreply.github.com>
2025-01-16 16:18:06 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,30 +1,11 @@
-/*
-_  ______   _____ _____ _____ _   _
-| |/ / ___| |_   _| ____/___ | | | |
-| ' / |  _    | | |  _|| |   | |_| |
-| . \ |_| |   | | | |__| |___|  _  |
-|_|\_\____|   |_| |_____\____|_| |_|
-
-ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
-CONTACT ME HERE +237656520674
-YT: KermHackTools
-Github: Kgtech-cmr
-*/
-
-
-
-
-
-
-
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "ai",
-    alias: ["gpt","bot"], 
-    react: "🧠",
+    alias: ["a","gpt"], 
+    react: "🤖",
     desc: "ai chat.",
     category: "main",
     filename: __filename

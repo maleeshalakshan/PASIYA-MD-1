dcada067bd7867a67a51921b0d7ac479b20bfb9d
Maleesha Lakshan <109362145+maleeshalakshan@users.noreply.github.com>
2025-01-18 22:37:25 +0530

    Update MOVIEmenu.js

--- plugins/MOVIEmenu (1).js
+++ plugins/MOVIEmenu (1).js
@@ -1,20 +1,10 @@
-/*created by Kgtech 🕵
-contact dev1 237656520674 ♻️
-contact dev2 237650564445 ♻️
-© Copy coder alert ⚠
-*/
-
-
-
-
-
 const config = require('../config')
 const {cmd , commands} = require('../command')
 cmd({
 
     pattern: "menu4",
 
-    react: "📑",
+    react: "🧾",
 
     alias: ["panel","list","commands"],
 
